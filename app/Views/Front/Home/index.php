f1b468829f8f93d16682b60ecad66ab7d14e973b
luciomdesouza <luciomdesouza@hotmail.com>
2023-07-15 21:26:51 -0300

    02 - Criando algumas coisas na home

--- app/Views/Front/Home/index.php
+++ app/Views/Front/Home/index.php
@@ -17,14 +17,130 @@
 <?php echo $this->section('content'); ?>
 
 
-<!-- Begin Page Content -->
-<div class="container-fluid">
+<div class="container pt-5 text-center">
+    <h1 class="mt-5">Veja como é fácil criar o seu agendamento</h1>
 
-    <!-- Page Heading -->
-    <h1 class="h3 mb-4 text-gray-800"><?php echo $title ?? 'Home'; ?></h1>
+
+    <div class="row mt-4">
+
+        <div class="col">
+
+            <div class="card">
+
+                <div class="card-header">
+                    Primeiro
+                </div>
+
+                <div class="card-body">
+
+                    <h5 class="card-title">Autentique-se</h5>
+                    <p class="card-text">Realize o login ou crie a sua conta</p>
+
+                </div>
+
+
+            </div>
+
+        </div>
+
+
+        <div class="col">
+
+            <div class="card">
+
+                <div class="card-header">
+                    Segundo
+                </div>
+
+                <div class="card-body">
+
+                    <h5 class="card-title">Escolha a Unidade</h5>
+                    <p class="card-text">Onde você gostaria de ser atendido</p>
+
+                </div>
+
+
+            </div>
+
+        </div>
+
+
+
+        <div class="col">
+
+            <div class="card">
+
+                <div class="card-header">
+                    Terceiro
+                </div>
+
+                <div class="card-body">
+
+                    <h5 class="card-title">Escolha o Serviço</h5>
+                    <p class="card-text">Serviço que deseja atendimento</p>
+
+                </div>
+
+
+            </div>
+
+        </div>
+
+
+        <div class="col">
+
+            <div class="card">
+
+                <div class="card-header">
+                    Quarto
+                </div>
+
+                <div class="card-body">
+
+                    <h5 class="card-title">Escolha a data</h5>
+                    <p class="card-text">Escolha a melhor data e horário</p>
+
+                </div>
+
+
+            </div>
+
+        </div>
+
+
+        <div class="col">
+
+            <div class="card">
+
+                <div class="card-header">
+                    Pronto
+                </div>
+
+                <div class="card-body">
+
+                    <h5 class="card-title">Confirmação</h5>
+                    <p class="card-text">Revise os dados e crie o agendamento</p>
+
+                </div>
+
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="row mt-4">
+
+        <div class="col-m-12">
+
+            <a href="<?php echo route_to('schedule.new'); ?>" class="btn btn-lg btn-primary">Criar agendamento</a>
+
+        </div>
+
+    </div>
 
 </div>
-<!-- /.container-fluid -->
 
 
 <?php echo $this->endSection(); ?>
--- app/Views/Front/Layout/main.php
+++ app/Views/Front/Layout/main.php
@@ -52,14 +52,14 @@
         <!-- Fixed navbar -->
         <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Fixed navbar</a>
+                <a class="navbar-brand" href="<?php echo route_to('home'); ?>">Meus agendamentos</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <ul class="navbar-nav me-auto mb-2 mb-md-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link active" aria-current="page" href="<?php echo route_to('home'); ?>">Home</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Link</a>
@@ -83,11 +83,7 @@
 
         <?php echo $this->renderSection('content'); ?>
 
-        <div class="container">
-            <h1 class="mt-5">Sticky footer with fixed navbar</h1>
-            <p class="lead">Pin a footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code class="small">padding-top: 60px;</code> on the <code class="small">main &gt; .container</code>.</p>
-            <p>Back to <a href="/docs/5.0/examples/sticky-footer/">the default sticky footer</a> minus the navbar.</p>
-        </div>
+
     </main>
 
     <footer class="footer mt-auto py-3 bg-light">
