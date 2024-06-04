6efb195b30e5c4e644684e020a45a4352407cb4b
wilker22 <wilker.silva@codevasf.gov.br>
2024-06-04 16:04:30 -0300

    escape data

--- app/Models/UnitModel.php
+++ app/Models/UnitModel.php
@@ -62,14 +62,24 @@ class UnitModel extends Model
 
     // Callbacks
     protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
+    protected $beforeInsert   = ['escapeData'];
     protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
+    protected $beforeUpdate   = ['escapeData'];
     protected $afterUpdate    = [];
     protected $beforeFind     = [];
     protected $afterFind      = [];
     protected $beforeDelete   = [];
     protected $afterDelete    = [];
+
+
+    protected function escapeData(array $data)
+    {
+        if (!isset($data['data'])) {
+            return $data;
+        }
+
+        return esc($data);
+    }
 
     public function findOrFail(int|string $id): object
     {
